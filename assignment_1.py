07b438d03a9122cfbfa46398a81494dd0cecb277
gabefiol <83429054+gabefiol@users.noreply.github.com>
2026-05-06 14:12:53 -0700

    updated workshop file and added first two assignment files

---
+++ assignment_1.py
@@ -0,0 +1,13 @@
+############### Assignment 1 ###############
+
+# Basic Data Structures
+
+# 1. Create a new variable that contains the name of each of your classes
+
+
+# 2. Create a list that contains a range of numbers from 1 to the total number of classes you have
+
+
+# 3. Create a dictionary using the list and the class variables where the key is the order of when you have each class in a week (i.e. psych 101 is the first class in the week on Monday so this would correspond to 1)
+    # Pull the numbers by indexing from the list and input the string variables you defined earlier for the values.
+    # ex. {1 : "Psych 101"}
---
+++ assignment_2.py
@@ -0,0 +1,17 @@
+########### Assignment 2 ###########
+
+# Data Structures cont. + Basic Functions
+# Using the dictionary from the last assignment: 
+
+# 1. Print the list of keys in the dictionary. Then, print the list of values in the dictionary.
+    # Output (keys): dict_keys([1,2,3])
+    # Output (values): dict_values(['psych 101, 'psych 102', 'psych 103'])
+    
+    # 1a. Print the number of the keys and the number of the values using the len() function.
+        # Output (same for both): 3
+        
+# 2. Using conditional statements, check if the first 5 letters of the first class in your dictionary is 'Psych'; if True, print "this is a psych class" and 
+    # if False, print "this is not a psych class"
+
+# 3. Using nested conditional statements or "and" statements, check if there are at least 2 psych classes in your dictionary. If true, print "you may be a psych major" and 
+    # if false, print "you may not be a psych major"
